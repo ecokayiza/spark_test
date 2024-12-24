22b5991ce554cc93c2bfee40e91d69713ef9b796
Ecoka <130556432+ecokayiza@users.noreply.github.com>
2024-12-24 21:01:13 +0800

    Update risk_assessment.py

--- design/pages/risk_assessment.py
+++ design/pages/risk_assessment.py
@@ -23,14 +23,17 @@ template = """
 zhipu_api_key="6328b91e5a03d4284108b0f6065f936e.qa0IS6bGvUmeRp8f"
 os.environ["ZHIPUAI_API_KEY"] = zhipu_api_key
 
+@st.cache_data
+def get_AI():
+    client = ZhipuAI(api_key=zhipu_api_key) 
 
-client = ZhipuAI(api_key=zhipu_api_key) 
+    file_01 = client.files.create(file=Path("design/result/llmdata/year_landings_addr.csv"), purpose="file-extract")
+    content_01 = json.loads(client.files.content(file_01.id).content)["content"]
 
-file_01 = client.files.create(file=Path("design/result/llmdata/year_landings_addr.csv"), purpose="file-extract")
-content_01 = json.loads(client.files.content(file_01.id).content)["content"]
-
-file_02 = client.files.create(file=Path("design/result/llmdata/year_season_typhoon.csv"), purpose="file-extract")
-content_02 = json.loads(client.files.content(file_02.id).content)["content"]
+    file_02 = client.files.create(file=Path("design/result/llmdata/year_season_typhoon.csv"), purpose="file-extract")
+    content_02 = json.loads(client.files.content(file_02.id).content)["content"]
+    return client,content_01,content_02
+client,content_01,content_02=get_AI()
 
 
 
